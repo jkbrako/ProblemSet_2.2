1485ead9049da23d37ee0e23a24ec5403263ff46
Jamin Brako <jbrako@ucvts.tec.nj.us>
2020-01-31 13:23:08 -0500

    lol

--- src/problemset2_2/ProblemSet2_2.java
+++ src/problemset2_2/ProblemSet2_2.java
@@ -4,8 +4,15 @@ public class ProblemSet2_2 {
     public static void main(String[] args) {
         //Use print statements to create your own beautiful original ASCII art.
         //Use comments to describe what your image is depicting.
-        
-        
+        System.out.println("         .-\"\"\"-.");       
+        System.out.println("        /       \\");       
+        System.out.println("        \\       /");       
+        System.out.println(" .-\"\"\"-.-`.-.-.<  _");       
+        System.out.println("/      _,-\\ ()()_/:@");       
+        System.out.println("\\     / ,  `     `|");       
+        System.out.println(" '-..-| \\-.,___,  /");       
+        System.out.println("       \\ `-.__/  /");       
+        System.out.println("  jgs   `-.__.-'`");       
         
         
         
